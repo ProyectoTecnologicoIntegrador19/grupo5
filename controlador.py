7675a52141fe86ec866e6ebeb6775110088aa833
braian <braianpalacio2001@gmail.com>
2023-09-30 11:59:32 -0300

    merge con cristian

--- controlador.py
+++ controlador.py
@@ -1,5 +1,6 @@
 import developer
 import tendencias
+import perfil
 
 def listarTendenciasMensuales():
         url= 'https://github.com/trending?since=monthly&spoken_language_code=es' # link de pagina a scrapear
@@ -74,4 +75,43 @@ def listarTendDeveloperDiarias():
                 print(f'Título Repositorio: {t["Título Repositorio"]}')
                 print(f'Descripción Repositorio: {t["Descripción Repositorio"]}')  
             
+#------------------------------------------------------------------------------------------------------------------------------------------------
+def listarInformacionPerfil():
+    url = input('\nIngrese nombre de usuario de perfil de GitHub: ')
+    con = perfil.PerfilModelo(url)
+    resultado= con.obtenerInfoPerfil(url)
+    #dividimos la tupla(resultado) en las 3 variables por orden de return
+    perfil_info, numero_contribuciones, repositorios_info, readme_info = resultado
+    
+     
+    if readme_info:
+        if 'titulo' in readme_info[0]:
+            print(f'\n\nReadme:  {readme_info[0]["titulo"]}')
+            for parrafo in readme_info[1:]:  # Se comienza desde el segundo elemento para no repetir el titulo
+                print(parrafo["Descripcion"])
+        # else:print('No se encontró un titulo\n')
+    else:print('\n\nNo se encontró un README\n')
+    
+    print('-' * 100)
+    for p in perfil_info:
+        print(f'\nUsuario: {p["Usuario"]}')
+        print(f'Descripcion: {p["Descripcion perfil"]}')
+        print(f'Seguidores: {p["Seguidores"]}')
+        print(f'Siguiendo: {p["Siguiendo"]}')
+        print('')
+        print('-' * 100)
+    
+    print(f'\nPopular repositories:')
+    for repo in repositorios_info:
+        print(f'\nRepositorio: {repo["Repositorios"]}')
+        print(f'Lenguaje: {repo["Lenguaje de Programación"]}')
+        print(f'Estrellas: {repo["Estrellas"]}')
+        print(f'Clonados: {repo["Clonados"]}')
+        print(f'Descripción: {repo["Descripcion"]}')
+        
+    
+    print('-' * 100)    
+    print(f'\nNúmero de Contribuciones: {numero_contribuciones}\n')
+       
+#----------------------------------------------------------------------------
             
--- vista.py
+++ vista.py
@@ -1,7 +1,7 @@
 import controlador
 
 while True:
-        menu = input("""\nIngrese número: 1) Tendencias   2) Tendencias Developer   9) Salir \n     """)
+        menu = input("""\nIngrese número: 1) Tendencias   2) Tendencias Developer   3) Perfil   9) Salir \n     """)
         if menu == "1":#TENDENCIAS-----------------------------------------------------------------------------------------------------
             menuTendencias = input("""\nTendencias:  1) Mensual   2) Semanal   3) Diaria \n     """)
             if menuTendencias == "1": controlador.listarTendenciasMensuales ()#llama al archivo controlador y el metodo que que se necesita mostrar
@@ -12,7 +12,8 @@ while True:
             if menuTendDeveloper == "1": controlador.listarTendDeveloperMensuales ()
             if menuTendDeveloper == "2": controlador.listarTendDeveloperSemanales ()
             if menuTendDeveloper == "3": controlador.listarTendDeveloperDiarias ()
-            
+        elif menu == "3":#PERFIL-----------------------------------------------------------------------------------------------------
+            controlador.listarInformacionPerfil ()    
         
         elif menu == "9":#SALIR    
             print("\n OFF."); exit()
