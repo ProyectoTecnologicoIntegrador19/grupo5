58ba5c98f1e4ea4f913c7216de572749b1914b2a
braian <braianpalacio2001@gmail.com>
2023-09-30 12:05:57 -0300

    arreglos

--- controlador.py
+++ controlador.py
@@ -3,46 +3,50 @@ import tendencias
 import perfil
 
 def listarTendenciasMensuales():
-        url= 'https://github.com/trending?since=monthly&spoken_language_code=es' # link de pagina a scrapear
-        #cantidad = int(input("\nIngrese número de repositorios a extraer: "))
-        #variable(listaRepositorios) donde llamamos al archivo(tendencias) donde tenemos la clase que necesitamos(TendenciasModelo) y le mandamos la url
-        listaRepositorios = tendencias.TendenciasModelo(url) 
-        #variable(lista) donde llamamos la variable anterior(listaRepositorios) y llamamos al metodo (obtenerTendencias) con la url
-        lista = listaRepositorios.obtenerTendencias(url)
-        #recorremos los resultados que nos trajo con un for
-        for t in lista:
-                print(f'\nRepositorio: {t["Repositorio"]}')
-                print(f'Desarrollador: {t["Desarrollador"]}')
-                print(f'Descripción: {t["Descripción"]}')
-                print(f'Lenguaje de Programación: {t["Lenguaje de Programación"]}')
-                print(f'Estrellas: {t["Estrellas"]}' + ' ' * 50 + f'Clonados: {t["Clonados"]}\n')
-                print('-' * 100)
-           
+    # link de pagina a scrapear
+    url = 'https://github.com/trending?since=monthly&spoken_language_code=es'
+    cantidad = int(input("\nIngrese número de repositorios a extraer(0 para extraer todos): "))
+    # variable(con) donde llamamos al archivo(tendencias) donde tenemos la clase que necesitamos(TendenciasModelo) y le mandamos la url
+    con = tendencias.TendenciasModelo(url, cantidad)
+    # variable(lista) donde llamamos la variable anterior(con) y llamamos al metodo (obtenerTendencias) con la url
+    lista = con.obtenerTendencias(url, cantidad)
+    # recorremos los resultados que nos trajo con un for
+    for t in lista:
+        print(f'\nRepositorio: {t["Repositorio"]}')
+        print(f'Desarrollador: {t["Desarrollador"]}')
+        print(f'Descripción: {t["Descripción"]}')
+        print(f'Lenguaje de Programación: {t["Lenguaje de Programación"]}')
+        print(f'Estrellas: {t["Estrellas"]}' + ' ' *50 + f'Clonados: {t["Clonados"]}\n')
+        print('-' * 100)
+
 
 def listarTendenciasSemanales():
-        url='https://github.com/trending?since=weekly&spoken_language_code=es' # link de pagina a scrapear
-        listaRepositorios = tendencias.TendenciasModelo(url)
-        lista = listaRepositorios.obtenerTendencias(url)
-        for t in lista:
-            print(f'\nRepositorio: {t["Repositorio"]}')
-            print(f'Desarrollador: {t["Desarrollador"]}')
-            print(f'Descripción: {t["Descripción"]}')
-            print(f'Lenguaje de Programación: {t["Lenguaje de Programación"]}')
-            print(f'Estrellas: {t["Estrellas"]}' + ' ' * 50 + f'Clonados: {t["Clonados"]}\n')
-            print('-' * 100)
-            
+    url = 'https://github.com/trending?since=weekly&spoken_language_code=es'
+    cantidad = int(input("\nIngrese número de repositorios a extraer(0 para extraer todos): "))
+    con = tendencias.TendenciasModelo(url, cantidad)
+    lista = con.obtenerTendencias(url, cantidad)
+    for t in lista:
+        print(f'\nRepositorio: {t["Repositorio"]}')
+        print(f'Desarrollador: {t["Desarrollador"]}')
+        print(f'Descripción: {t["Descripción"]}')
+        print(f'Lenguaje de Programación: {t["Lenguaje de Programación"]}')
+        print(f'Estrellas: {t["Estrellas"]}' + ' ' *50 + f'Clonados: {t["Clonados"]}\n')
+        print('-' * 100)
+
+
 def listarTendenciasDiarias():
-        url='https://github.com/trending?since=daily&spoken_language_code=es' # link de pagina a scrapear
-        listaRepositorios = tendencias.TendenciasModelo(url)
-        lista = listaRepositorios.obtenerTendencias(url)
-        for t in lista:
-            print(f'\nRepositorio: {t["Repositorio"]}')
-            print(f'Desarrollador: {t["Desarrollador"]}')
-            print(f'Descripción: {t["Descripción"]}')
-            print(f'Lenguaje de Programación: {t["Lenguaje de Programación"]}')
-            print(f'Estrellas: {t["Estrellas"]}' + ' ' * 50 + f'Clonados: {t["Clonados"]}\n')
-            print('-' * 100)
-        
+    url = 'https://github.com/trending?since=daily&spoken_language_code=es'
+    cantidad = int(input("\nIngrese número de repositorios a extraer(0 para extraer todos): "))
+    con = tendencias.TendenciasModelo(url, cantidad)
+    lista = con.obtenerTendencias(url, cantidad)
+    for t in lista:
+        print(f'\nRepositorio: {t["Repositorio"]}')
+        print(f'Desarrollador: {t["Desarrollador"]}')
+        print(f'Descripción: {t["Descripción"]}')
+        print(f'Lenguaje de Programación: {t["Lenguaje de Programación"]}')
+        print(f'Estrellas: {t["Estrellas"]}' + ' ' *50 + f'Clonados: {t["Clonados"]}\n')
+        print('-' * 100)
+
 #--------------------------------------------------------------------------------------------------------------------
 
 def listarTendDeveloperMensuales():
