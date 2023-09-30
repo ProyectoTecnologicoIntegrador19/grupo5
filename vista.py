0728cfaa89cd5e8bd259613d0d4ba455d0cd2769
Cristian-Kakao <131078780+Cristian-Kakao@users.noreply.github.com>
2023-09-30 11:44:40 -0300

    Add files via upload

--- vista.py
+++ vista.py
@@ -1,20 +1,21 @@
-import controlador 
+import controlador
 
 while True:
-        menu = input("""\nIngrese número: 1) Tendencias      8) Perfil   9) Salir \n     """)
+        menu = input("""\nIngrese número: 1) Tendencias   2) Tendencias Developer   9) Salir \n     """)
         if menu == "1":#TENDENCIAS-----------------------------------------------------------------------------------------------------
-            menuTendencias = input("""\nTendencias:  1) Mensual   2) Semanal   3) Diaria \n             """)
+            menuTendencias = input("""\nTendencias:  1) Mensual   2) Semanal   3) Diaria \n     """)
             if menuTendencias == "1": controlador.listarTendenciasMensuales ()#llama al archivo controlador y el metodo que que se necesita mostrar
             if menuTendencias == "2": controlador.listarTendenciasSemanales ()
             if menuTendencias == "3": controlador.listarTendenciasDiarias ()
-        if menu == "8":#PERFIL-----------------------------------------------------------------------------------------------------
-            controlador.listarInformacionPerfil ()
+        elif menu == "2":#TENDENCIAS DEVELOPER-------------------------------------------------------------------------------------------
+            menuTendDeveloper = input("""\nTendencias Developer:  1) Mensual   2) Semanal   3) Diaria \n     """)
+            if menuTendDeveloper == "1": controlador.listarTendDeveloperMensuales ()
+            if menuTendDeveloper == "2": controlador.listarTendDeveloperSemanales ()
+            if menuTendDeveloper == "3": controlador.listarTendDeveloperDiarias ()
+            
         
         elif menu == "9":#SALIR    
             print("\n OFF."); exit()
-        else: 
-            print("\nOpcion no valida")
+        else: print("\nOpcion no valida")
         
-        
-           
         
